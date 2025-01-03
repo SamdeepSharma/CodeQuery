9a510eeb5283533f950c208b3f7f87f462c522fc
Samdeep Sharma <sharmasamdeep1@gmail.com>
2025-01-03 23:32:12 +0530

    made appwrite configs for client and server

---
+++ src/utils/name.ts
@@ -0,0 +1,8 @@
+const db = "main-db"
+const questionCollection = "questions"
+const answerCollection = "answers"
+const commentCollection = "comments"
+const voteCollection = "votes"
+const questionAttachmentBucket = "question-attachments"
+
+export {db, questionCollection, answerCollection, commentCollection, voteCollection, questionAttachmentBucket}
